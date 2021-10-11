9befd3f8cebcb21bc0688ffb6589152fbb8f2311
krystekswag <krystekswag@gmail.com>
2021-10-11 14:43:25 +0200

    Set template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import MainPage from './views/Main-page.vue';
+
+export default {
+  name: 'App',
+  components: MainPage,
+}
+</script>
+
+<style lang="scss">
+
+</style>
