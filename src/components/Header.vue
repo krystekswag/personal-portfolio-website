87b597ea30479ddca33eecb8aa4a5ab792f8283a
krystekswag <krystekswag@gmail.com>
2021-10-26 14:58:56 +0200

    add mobile menu and set responsives

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,10 +14,10 @@
                     </svg>
                 </a>
             </div>
-            <div class="nav-wrapper">
+            <div class="nav-wrapper" id="navigation">
                 <ul>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#portfolio" class="nav-link">Portfolio</a></li>
+                    <li><a href="#contact" class="nav-link">Contact</a></li>
                 </ul>
             </div>
             <div class="social-wrapper">
@@ -31,7 +31,14 @@
                     <li><a href="https://www.behance.net/krystekswag" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" class="social-icon" width="24" height="24" viewBox="0 0 24 24"><path d="M18.003 12.333h-3.018c.289-1.977 2.902-1.715 3.018 0zm-9.742.667h-2.261v2.01h2.228c2.036 0 1.911-2.01.033-2.01zm-.08-4h-2.181v2h2.389c1.673 0 1.938-2-.208-2zm15.819-9v24h-24v-24h24zm-10 8h5v-1h-5v1zm-3.552 3.618c1.907-.974 1.837-4.55-1.813-4.604h-4.635v9.978h4.311c4.522 0 4.445-4.534 2.137-5.374zm9.487.602c-.274-1.763-1.529-2.95-3.583-2.95-2.095 0-3.352 1.34-3.352 3.947 0 2.631 1.368 3.783 3.417 3.783 2.048 0 3.106-1.135 3.4-2h-2.112c-.737.855-2.893.521-2.767-1.353h5.059c.01-.634-.011-1.089-.062-1.427z"/></svg>
                     </a></li>
-                </ul>                
+                </ul>             
+            </div>
+            <div id="hamburger">
+                <ul>
+                    <li><a id="mobile-hamburger">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="social-icon hamburger" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+                    </a></li>
+                </ul> 
             </div>
         </div>
     </div>
@@ -40,6 +47,33 @@
 <script>
     export default {
         name: 'Header',
+        mounted() {
+            this.showMobileMenu();
+            this.hideMenu();
+        },
+        methods: {
+            showMobileMenu(){
+                const buttonMobile = document.getElementById("mobile-hamburger");
+                const menu = document.getElementById("navigation");
+                buttonMobile.addEventListener('click', function(){
+                    if(menu.style.display == "block"){
+                        menu.style.display = "none";
+                    }
+                    else{
+                        menu.style.display = "block";
+                    }
+                })
+            },
+            hideMenu(){
+                const menu = document.getElementById("navigation");
+                const links = document.querySelectorAll(".nav-link");
+                    links.forEach((e)=>{
+                        e.addEventListener('click', function(){
+                            menu.style.display = "none";
+                        })
+                    })
+            }
+        }
     }
 </script>
 
@@ -50,7 +84,7 @@
         border-bottom: 1px solid #ebeaea;
         width: 100%;
         position: fixed;
-        z-index: 4;
+        z-index: 5;
     }
     .container{
         height: 100%;
@@ -62,8 +96,16 @@
         width: 45px;
         height: 28px;
     }
-    #logo:hover{
-        fill: #ed1a4d;
+    @media only screen and (min-width: 706px){
+        #logo:hover{
+            fill: #ed1a4d;
+        }
+        .nav-wrapper ul li a:hover{
+            border-bottom: 3px solid #ed1a4d;
+        }
+        .social-icon:hover{
+            fill: #ed1a4d;
+        }
     }
     .logo-wrapper{
         position: relative;
@@ -71,19 +113,20 @@
         width: calc(100% / 3);
     }
     .logo-wrapper a{
-        margin-left: 40%;
+        margin-left: 35%;
     }
     .nav-wrapper{
         display: inline-block;
         width: calc(100% / 3);
+        text-align: center;
     }
-    .nav-wrapper ul, .social-wrapper ul{
+    .nav-wrapper ul, .social-wrapper ul, #hamburger ul{
         display: inline-block;
         list-style-type: none;
     }
     .social-wrapper ul{
         float: right;
-        margin-right: 40%;
+        margin-right: 35%;
     }
     .nav-wrapper ul li, .social-wrapper ul li{
         display: inline;
@@ -94,9 +137,6 @@
         color: #333333;
         font-size: 18px;
     }
-    .nav-wrapper ul li a:hover{
-        border-bottom: 3px solid #ed1a4d;
-    }
     .social-wrapper{
         display: inline-block;
         width: calc(100% / 3);
@@ -106,7 +146,73 @@
         height: 30px;
         width: 30px;
     }
-    .social-icon:hover{
-        fill: #ed1a4d;
+    .hamburger{
+        display: none;
+    }
+    @media only screen and (max-width: 1250px){
+        .nav-wrapper ul li, .social-wrapper ul li{
+            padding: 5px 7px;
+        }
+        .logo-wrapper a{
+            margin-left: 20%;
+        }
+        .social-wrapper ul{
+            float: right;
+            margin-right: 20%;
+        }
+    }
+    @media only screen and (max-width: 705px){
+        .logo-wrapper{
+            width: 50%;
+        }
+        .logo-wrapper a{
+            margin-left: 20%;
+        }
+        #hamburger{
+            width: 50%;
+        }
+        #hamburger ul{
+            float: right;
+            margin-right: 20%;
+        }
+        .hamburger{
+            display: block;
+        }
+        #mobile-hamburger{
+            cursor: pointer;
+        }
+        .nav-wrapper{
+            display: none;
+            position: absolute;
+            background-color: #333333;
+            text-align: center;
+            width: 100%;
+            top: 70px;
+            margin-bottom: 70px;
+            animation: displayContent .5s;
+        }
+        .nav-wrapper ul li a{
+            color: #f1f1f1;
+            font-size: 22px;
+        }
+        .nav-wrapper ul{
+            padding: 0;
+        }
+        .social-wrapper{
+            display: none;
+        }
+        .nav-wrapper ul li, .social-wrapper ul li{
+            display: block;
+        }
+        @keyframes displayContent{
+            0%{
+                transform: translate(0, -100px);
+                z-index: 4;
+                opacity: 0;
+            }
+            100%{
+                transform: translate(0, 0);
+            }
+        }
     }
 </style>
