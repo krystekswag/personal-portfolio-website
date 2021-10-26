6c0a31974423b5cee2822ecf0b74d80d8d97d6b8
krystekswag <krystekswag@gmail.com>
2021-10-26 14:57:02 +0200

    add error 404 component

---
+++ src/components/Err404.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <p>404 ERROR - back to <a href="https://krystekswag.eu">page</a></p>
+        <svg version="1.1" alt="ERROR404 text" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	viewBox="0 0 600 252" style="enable-background:new 0 0 600 252;" xml:space="preserve">
+<g>
+	<polyline class="st0" points="459,28.7 500.7,15.3 554.5,116.1 453.8,250.7 445.5,200 512.1,117.5 	"/>
+	<polyline class="st0" points="141,223.3 99.3,236.8 45.5,135.9 146.2,1.3 154.5,52 87.9,134.5 	"/>
+</g>
+<g>
+	<path class="st0" d="M328.8,171.7l3-86.7L275,55.9l-14.8,19.5l10.9,4.9l-3,86.7l56.8,29.1l14.8-19.5L328.8,171.7z M283.9,86.1
+		l29,13.1l-4.3,59.1l7.5,7.7l-29-13.1l4.3-59.1L283.9,86.1z"/>
+	<polygon class="st0" points="238.1,153 216.4,143.2 234.9,75.3 214.3,80.5 199,135.4 185.5,129.2 189.7,70.2 169.8,49.6 
+		166.5,143.4 193,157 180.3,202.5 202.4,194.7 210.3,165.8 223.3,172.5 	"/>
+	<polygon class="st0" points="433.5,153 411.8,143.2 430.3,75.3 409.7,80.5 394.4,135.4 380.9,129.2 385.1,70.2 365.1,49.6 
+		361.9,143.4 388.4,157 375.7,202.5 397.8,194.7 405.7,165.8 418.7,172.5 	"/>
+</g>
+</svg>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Err404',
+    }
+</script>
+
+<style lang="scss" scoped>
+    .container{
+        background-color: #f1f1f1;
+        height: auto;
+        padding: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .st0{
+        fill:#333333;
+    }
+</style>
